12feaafd91c0015039be610a83af0acc64cff66b
JamesE-J <39918662+JamesE-J@users.noreply.github.com>
2019-02-06 13:36:34 +0000

    Init

---
+++ location/location/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+public class location
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int c, i;
+            int hLocation = 0;
+            int pLocation = 0;
+            int timeout = 1000;
+            string address = "localhost";
+            string http = "";
+            bool hUsed = false;
+            bool pUsed = false;
+            List<string> temp = new List<string>();
+            int port = 43;
+            for (i = 0; i < args.Length; i++)
+            {
+                temp.Add(args[i]);
+            }
+            for (i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "/h")
+                {
+                    address = args[i + 1];
+                    hLocation = i;
+                    hUsed = true;
+                }
+                if (args[i] == "/p")
+                {
+                    port = Convert.ToInt32(args[i + 1]);
+                    pLocation = i;
+                    pUsed = true;
+                }
+                if (args[i] == "/h9")
+                {
+                    http = "/h9";
+                    temp.Remove("/h9");
+                }
+                if (args[i] == "/h0")
+                {
+                    http = "/h0";
+                    temp.Remove("/h0");
+                }
+                if (args[i] == "/h1")
+                {
+                    http = "/h1";
+                    temp.Remove("/h1");
+                }
+            }
+            if (pUsed == true && hUsed == true)
+            {
+                temp.RemoveRange(hLocation, 2);
+                temp.RemoveRange(pLocation, 2);
+            }
+            if (pUsed == true && hUsed == false)
+            {
+                temp.RemoveRange(pLocation, 2);
+            }
+            if (hUsed == true && pUsed == false)
+            {
+                temp.RemoveRange(hLocation, 2);
+            }
+            args = temp.ToArray();
+            TcpClient client = new TcpClient();
+            client.Connect(address, port);
+            client.ReceiveTimeout = timeout;
+            client.SendTimeout = timeout;
+            StreamWriter sw = new StreamWriter(client.GetStream());
+            StreamReader sr = new StreamReader(client.GetStream());
+            switch (http)
+            {
+                default :
+                    if (args.Length == 2)
+                    {
+                        sw.WriteLine(args[0] + " " + args[1]);
+                        sw.Flush();
+                        string line = sr.ReadLine().Trim();
+                        Console.WriteLine(args[0] + " location changed to be in " + args[1]);
+                        break;
+                    }
+                    else
+                    {
+                        sw.WriteLine(args[0]);
+                        sw.Flush();
+                        string line = sr.ReadLine().Trim();
+                        if (line == "ERROR: no entries found")
+                        {
+                            Console.WriteLine("ERROR: no entries found");
+                        }
+                        else
+                        {
+                            Console.WriteLine(args[0] + " is in " + line);
+                        }
+                        break;
+                    }
+
+                case "/h9":
+                    if (args.Length == 2)
+                    {
+                        sw.WriteLine("PUT /" + args[0]);
+                        sw.WriteLine("");
+                        sw.WriteLine(args[1]);
+                        sw.Flush();
+                        Console.WriteLine(args[0] + " is now in " + args[1]);
+                        break;
+                    }
+                    else
+                    {
+                        sw.WriteLine("GET /" + args[0]);
+                        sw.Flush();
+                        string line = sr.ReadLine().Trim();
+                        String[] segments = line.Split(new char[] { ' ' }, 4);
+                        if (segments[1] == "404")
+                        {
+                            Console.WriteLine("ERROR: no entries found");
+                        }
+                        else
+                        {
+                            line = sr.ReadLine().Trim();
+                            line = sr.ReadLine().Trim();
+                            line = sr.ReadLine().Trim();
+                            Console.WriteLine(args[0] + " is in " + line);
+                        }
+                        break;
+                    }
+
+                case "/h0":
+                    if (args.Length == 2)
+                    {
+                        sw.WriteLine("POST /" + args[0] + " HTTP/1.0");
+                        sw.WriteLine("Content-Length: " + args[1].Length);
+                        sw.WriteLine("");
+                        sw.WriteLine(args[1]);
+                        sw.Flush();
+                        Console.WriteLine(args[0] + " is now in " + args[1]);
+                        break;
+                    }
+                    else
+                    {
+                        sw.WriteLine("GET /?" + args[0] + " HTTP/1.0");
+                        sw.WriteLine("");
+                        sw.Flush();
+                        string line = sr.ReadLine().Trim();
+                        String[] segments = line.Split(new char[] { ' ' }, 4);
+                        if (segments[1] == "404")
+                        {
+                            Console.WriteLine("ERROR: no entries found");
+                        }
+                        else
+                        {
+                            line = sr.ReadLine().Trim();
+                            line = sr.ReadLine().Trim();
+                            line = sr.ReadLine().Trim();
+                            Console.WriteLine(args[0] + " is in " + line);
+                        }
+                        break;
+                    }
+
+                case "/h1":
+                    if (args.Length == 2)
+                    {
+                        string sentName = "name=";
+                        string sentLocation = "&location=";
+                        int contentLength = 
+                            sentName.Length + sentLocation.Length + args[0].Length + args[1].Length;
+                        sw.WriteLine("POST / HTTP/1.1");
+                        sw.WriteLine("Host: " + address);
+                        sw.WriteLine("Content-Length: " + contentLength);
+                        sw.WriteLine();
+                        sw.WriteLine(sentName + args[0] + sentLocation + args[1]);
+                        sw.Flush();
+                        Console.WriteLine(args[0] + " is now in " + args[1]);
+                        break;
+                    }
+                    else
+                    {
+                        sw.WriteLine("GET /?name=" + args[0] + " HTTP/1.1");
+                        sw.WriteLine("Host: " + address);
+                        sw.WriteLine("");
+                        sw.Flush();
+                        string line = sr.ReadLine().Trim();
+                        String[] segments = line.Split(new char[] { ' ' }, 4);
+                        if (segments[1] == "404")
+                        {
+                            Console.WriteLine("ERROR: no entries found");
+                        }
+                        else
+                        {
+                            line = sr.ReadLine().Trim();
+                            line = sr.ReadLine().Trim();
+                            line = sr.ReadLine().Trim();
+                            Console.WriteLine(args[0] + " is in " + line);
+                        }
+                        break;
+                    }
+            } 
+        }
+        catch
+        {
+            Console.WriteLine("Exception thrown");
+        }
+    }
+}
---
+++ locationserver/locationserver/Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace locationserver
+{
+    class Program
+    {
+        /// <summary>
+        /// This method is called from the main as soon at the start to 
+        /// create a server and to start listening for connections from a client.
+        /// </summary>
+        static void runServer(Dictionary<string, string> dict)
+        {
+            TcpListener listener;
+            Socket connection;
+            Handler RequestHandler;
+            try
+            {
+                //Create a TCP Socket
+                listener = new TcpListener(IPAddress.Any, 43);
+                listener.Start();
+                Console.WriteLine("Server Is Listening For Connection");
+                //listens for a connection and loops forever
+                while (true)
+                {
+                    connection = listener.AcceptSocket();
+                    RequestHandler = new Handler();
+                    Thread t = new Thread(() => RequestHandler.doRequest(connection, dict));
+                    t.Start();
+                }
+            } catch (Exception e)
+            {
+                //Catches exception
+                Console.WriteLine("Exception: " + e.ToString());
+            }
+        }
+        /// <summary>
+        /// This is the main, it is the main.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dict =
+            new Dictionary<string, string>();
+            dict.Add("505520", "is being tested");
+            runServer(dict);
+        }
+    }
+}
+class Handler
+{
+    public void doRequest(Socket connection, Dictionary<string, string> dict)
+    {
+        NetworkStream socketStream;
+        socketStream = new NetworkStream(connection);
+        Console.WriteLine("---CONNECTION STARTED---");
+        try
+        {
+            StreamWriter sw = new StreamWriter(socketStream);
+            StreamReader sr = new StreamReader(socketStream);
+            String line = sr.ReadLine().Trim();
+            Console.WriteLine("INPUT:" + line);
+            String[] segments = line.Split(new char[] { ' ' }, 4);
+            switch (segments[0])
+            {
+                case "GET":
+                    if (segments.Length == 2)
+                    {
+                        if (dict.ContainsKey(segments[1].TrimStart('/')))
+                        {
+                            sw.WriteLine("HTTP/0.9 200 OK");
+                            sw.WriteLine("Content-Type: text/plain");
+                            sw.WriteLine("");
+                            sw.WriteLine(dict[segments[1].TrimStart('/')]);
+                            sw.Flush();
+                        }
+                        else
+                        {
+                            sw.WriteLine("HTTP/0.9 404 Not Found");
+                            sw.WriteLine("Content-Type: text/plain");
+                            sw.WriteLine("");
+                            sw.Flush();
+                        }
+                        break;
+                    }
+                    switch (segments[2])
+                    {
+                        case "HTTP/1.0":
+                            if (dict.ContainsKey(segments[1].TrimStart('/', '?')))
+                            {
+                                sw.WriteLine("HTTP/1.0 200 OK");
+                                sw.WriteLine("Content-Type: text/plain");
+                                sw.WriteLine("");
+                                sw.WriteLine(dict[segments[1].TrimStart('/', '?')]);
+                                sw.Flush();
+                            }
+                            else
+                            {
+                                sw.WriteLine("HTTP/1.0 404 Not Found");
+                                sw.WriteLine("Content-Type: text/plain");
+                                sw.WriteLine("");
+                                sw.Flush();
+                            }
+                            break;
+                        case "HTTP/1.1":
+                            if (dict.ContainsKey(segments[1].Remove(0, 7)))
+                            {
+                                sw.WriteLine("HTTP/1.1 200 OK");
+                                sw.WriteLine("Content-Type: text/plain");
+                                sw.WriteLine("");
+                                sw.WriteLine(dict[segments[1].Remove(0, 7)]);
+                                sw.Flush();
+                            }
+                            else
+                            {
+                                sw.WriteLine("HTTP/1.1 404 Not Found");
+                                sw.WriteLine("Content-Type: text/plain");
+                                sw.WriteLine("");
+                                sw.Flush();
+                            }
+                            break;
+                    }
+                    break;
+                case "PUT":
+                    string name = segments[1].TrimStart('/');
+                    line = sr.ReadLine().Trim();
+                    line = sr.ReadLine().Trim();
+                    dict[name] = line;
+                    sw.WriteLine("HTTP/0.9 200 OK");
+                    sw.WriteLine("Content-Type: text/plain");
+                    sw.WriteLine("");
+                    sw.Flush();
+                    break;
+                case "POST":
+                    switch (segments[2])
+                    {
+                        case "HTTP/1.0":
+                            name = segments[1].TrimStart('/');
+                            line = sr.ReadLine().Trim();
+                            line = sr.ReadLine().Trim();
+                            line = sr.ReadLine().Trim();
+                            dict[name] = line;
+                            sw.WriteLine("HTTP/1.0 200 OK");
+                            sw.WriteLine("Content-Type: text/plain");
+                            sw.WriteLine("");
+                            sw.Flush();
+                            break;
+                        case "HTTP/1.1":
+                            line = sr.ReadLine().Trim();
+                            line = sr.ReadLine().Trim();
+                            line = sr.ReadLine().Trim();
+                            line = sr.ReadLine().Trim();
+                            String[] post1Segments = line.Split(new char[] { '&' }, 4);
+                            name = post1Segments[0].Remove(0, 5);
+                            dict[name] = post1Segments[1].Remove(0, 9);
+                            sw.WriteLine("HTTP/1.1 200 OK");
+                            sw.WriteLine("Content-Type: text/plain");
+                            sw.WriteLine("");
+                            sw.Flush();
+                            break;
+                    }
+                    break;
+                default:
+                    if (segments.Length > 1)
+                    {
+                        if (dict.ContainsKey(segments[0]))
+                        {
+                            string[] locationArray = segments.Skip(1).ToArray();
+                            string location = String.Join(" ", locationArray);
+                            dict[segments[0]] = location;
+                            sw.WriteLine("OK");
+                            sw.Flush();
+                        }
+                        else
+                        {
+                            string[] locationArray = segments.Skip(1).ToArray();
+                            string location = String.Join(" ", locationArray);
+                            dict.Add(segments[0], location);
+                            sw.WriteLine("OK");
+                            sw.Flush();
+                        }
+                    }
+                    else if (dict.ContainsKey(segments[0]))
+                    {
+                        sw.WriteLine(dict[segments[0]]);
+                        sw.Flush();
+                    }
+                    else
+                    {
+                        sw.WriteLine("ERROR: no entries found");
+                        sw.Flush();
+                    }
+                    break;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("ERROR: Something went wrong");
+        }
+        finally
+        {
+            socketStream.Close();
+            connection.Close();
+            Console.WriteLine("---CONNECTION ENDED---");
+        }
+    }
+}
